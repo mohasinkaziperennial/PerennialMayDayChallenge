867151da60212c12c048a71b9318030d2dbdf9e3
Mohasin Kazi <mohasin.kazi@perennialsys.com>
2019-05-16 18:42:18 +0530

    Second prob

--- Dev2.java
+++ Dev2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class Dev1{
+public class Dev2{
 
 	public static void main(String[] args) {
 
@@ -10,52 +10,43 @@ public class Dev1{
                 //System.out.println("Enter number of tests : ");
 		int testCount = sc.nextInt();
 		List<int[]> testCases = new ArrayList<>();
-		List<Integer> results = new ArrayList<>();
-		for(int test=0 ; test < testCount ; test++) {
-			//System.out.println("Enter arrayLength : ");
-			int arrayLength = sc.nextInt();
-                	//System.out.println("Enter result sum : ");
-               		int result  = sc.nextInt();
-                	int input[] = new int[arrayLength];
-                	for( int i = 0 ; i< arrayLength ; i++ ) {
-                		//System.out.println("Enter input number : "+(i+1));
-                		input[i] = sc.nextInt();
-                	}
-			testCases.add(input);
-			results.add(result);
-		}
 
-		for(int test = 0 ; test < testCount ; test++) {
-			int input[] = testCases.get(test);
-			int arrayLength = input.length;
-			int result = results.get(test);
-			boolean flag = false;
-			for( int i = 0 ; i< arrayLength ; i++ ) {
-				//System.out.println("Inside find loop : "+ input[i] );
-				int sum = 0; 
+		for(int test=0 ; test < testCount ; test++) {
+                        System.out.println("Enter arrayLength : ");
+                        int arrayLength = sc.nextInt();
+                        int input[] = new int[arrayLength];
+                        for( int i = 0 ; i< arrayLength ; i++ ) {
+                                System.out.println("Enter input number : "+(i+1));
+                                input[i] = sc.nextInt();
+                        }
+                        testCases.add(input);
+                }
+		
+		for( int[] input : testCases){
+			int  lenght  = input.length - 1;
+			
+
+			for(int i = 0; i <= length; i++) {
+
+				int sumLeft = 0;
+				int sumRight = 0;
 				for( int j = i ; j >= 0  ; j-- ) {
-					
-					sum += input[j];
-					//System.out.println("Insdie loop 1 sum : "+sum);
-					if (sum == result){ 
-                                        	System.out.println((j +1)+" "+(i+1));
-						//Result found flag
-						flag = true;
-						break;
-                                	} else if(sum > result) {
-						//System.out.println("Sum Greater, exiting ");
-						break;
+                                        sumLeft += input[j];
+				}
+
+				for( int j = length ; j >  i  ; j-- ) {
+                                        sumright += input[j];
+					if(sumLeft <= sumRight){
+						if( sumLeft == sumRight){
+							System.out.println(i+1 +" " length+1-1);
+						}	
+					break;
 					}
 				}
-				if(flag) {
-					
-					break;
-				}else{
-				//Continue
-				}
 			}
+
 		}
+
+
 	}
-
-
 }
